e76223a1edb6fde3b52efb65640d57ef59a7aed4
unknown <abaei1363@yahoo.com>
2023-02-12 12:29:14 +0330

    print

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -1,12 +1,20 @@
-<div style="font-family:Yekan;font-size: 20px" dir="rtl">
-    <h6>سامانه نوبت دهی انبار نفت شهید آنجفی</h6>
-    <h6>{{$loadrow->product_type}}</h6>
-    <h6>{{__('row')}} : {{$loadrow->row}}</h6>
-<h6>{{__('price')}} : {{$loadrow->product_price}} ریال</h6>
-<h6>{{__('car.number')}} : {{$loadrow->car_number}}</h6>
-<h6>{{__('driver.name')}} : {{$loadrow->driver_name}}</h6>
-<h6>{{__('membership.number')}} : {{$loadrow->membership_number}}</h6>
-<h6>{{__('issue.date')}} : {{$loadrow->issue_date}}</h6>
+<div style="font-family:'Yekan';font-size: 20px" dir="rtl" xmlns="http://www.w3.org/1999/html">
+    <h6 style="margin-right: 20px;font-size:22px">سامانه نوبت دهی </h6>
+<h6 style="margin-top:-40px;font-size:22px">        انبار نفت شهید آنجفی</h6>
+    <h6 style=" margin-top:-45px;margin-right: 20px;font-size: 40px;font-weight: bold">{{$loadrow->product_type}}</h6>
+    <div style="margin-top:-90px;font-size: 20px">
+        <label >{{__('row')}} :</label>
+        <em style="font-size:35px;font-weight: bold;"> {{$loadrow->row}}</em></div>
+<label style="font-size: 20px">{{__('price')}} : </label>
+    <em style="font-size:35px;font-weight: bold;">{{$loadrow->product_price}} ریال</em><br>
+    <label style="font-size: 20px">{{__('car.number')}} :</label>
+    <em style="font-size:30px;font-weight: bold;">{{$loadrow->car_number}}</em><br>
+    <lable style="font-size: 20px" >{{__('driver.name')}} : </lable>
+    <em style="font-size:30px;font-weight: bold;">{{$loadrow->driver_name}}</em><br>
+    <lable style="font-size: 20px">{{__('membership.number')}} : </lable>
+    <em style="font-size:30px;font-weight: bold;">{{$loadrow->membership_number}}</em><br>
+    <lable style="font-size: 20px" >{{__('issue.date')}} : </lable>
+    <em style="font-size:30px;font-weight: bold;"> {{$loadrow->issue_date}}</em><br>
 <h6>مدت اعتبار قبض {{$loadrow->expire_date}} روز از تاریخ صدور می باشد.</h6>
 </div>
 
