cba7df3c7f40107df206af0294504a03d6a00439
unknown <abaei1363@yahoo.com>
2023-02-13 12:39:47 +0330

    farsi

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,17 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Pishran\LaravelPersianString\HasPersianString;
 
 class Product extends Model
 {
     use HasFactory;
-    use HasPersianString;
-    protected $persianStrings = [
-        'name',
-        'price',
-        'expire_date',
-    ];
+
     public function user(){
         return $this->hasOne(User::class,'id','created_by');
 
